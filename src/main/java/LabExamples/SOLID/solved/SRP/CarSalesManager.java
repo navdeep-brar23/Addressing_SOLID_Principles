ef61a448c5635a150258bcde2417a70072d117c9
navdeep-brar23 <144741084+navdeep-brar23@users.noreply.github.com>
2024-11-25 02:13:41 -0700

    Update CarSalesManager.java

--- src/main/java/LabExamples/SOLID/solved/SRP/CarSalesManager.java
+++ src/main/java/LabExamples/SOLID/solved/SRP/CarSalesManager.java
@@ -1,5 +1,9 @@
 package main.java.LabExamples.SOLID.solved.SRP;
 
+/**
+ * CarSalesManager is responsible for handling car sales operations.
+ * This class adheres to the Single Responsibility Principle (SRP) by focusing solely on sales-related tasks.
+ */
 public class CarSalesManager {
     public void sellCar(String carModel) {
         System.out.println("Car sold: " + carModel);
