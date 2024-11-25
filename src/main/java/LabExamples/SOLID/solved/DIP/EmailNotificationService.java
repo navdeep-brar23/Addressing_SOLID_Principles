cb316a55fcdce86e9b0388e8523b66264fea5409
navdeep-brar23 <144741084+navdeep-brar23@users.noreply.github.com>
2024-11-25 02:07:43 -0700

    Update EmailNotificationService.java

--- src/main/java/LabExamples/SOLID/solved/DIP/EmailNotificationService.java
+++ src/main/java/LabExamples/SOLID/solved/DIP/EmailNotificationService.java
@@ -1,5 +1,8 @@
 package main.java.LabExamples.SOLID.solved.DIP;
-
+/**
+ * EmailNotificationService is an implementation of the NotificationService interface.
+ * It handles sending notifications via email.
+ */
 public class EmailNotificationService implements NotificationService {
     @Override
     public void sendNotification(String message) {
