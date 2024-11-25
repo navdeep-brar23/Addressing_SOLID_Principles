757246b2dfe66fd9f9250f52954572319ed81a3c
navdeep-brar23 <144741084+navdeep-brar23@users.noreply.github.com>
2024-11-25 02:13:59 -0700

    Update InvoiceManager.java

--- src/main/java/LabExamples/SOLID/solved/SRP/InvoiceManager.java
+++ src/main/java/LabExamples/SOLID/solved/SRP/InvoiceManager.java
@@ -1,5 +1,8 @@
 package main.java.LabExamples.SOLID.solved.SRP;
-
+/**
+ * InvoiceManager is responsible for generating invoices for car sales.
+ * This class adheres to the Single Responsibility Principle (SRP) by focusing solely on invoicing operations.
+ */
 public class InvoiceManager {
     public void generateInvoice(String carModel, double price) {
         System.out.println("Invoice generated for " + carModel + ", Amount: $" + price);
