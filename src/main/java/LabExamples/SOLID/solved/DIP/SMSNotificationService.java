1d79e6a2cc9e78bbbf0fa4ce8c23675505149e7d
navdeep-brar23 <144741084+navdeep-brar23@users.noreply.github.com>
2024-11-25 02:08:05 -0700

    Update SMSNotificationService.java

--- src/main/java/LabExamples/SOLID/solved/DIP/SMSNotificationService.java
+++ src/main/java/LabExamples/SOLID/solved/DIP/SMSNotificationService.java
@@ -1,5 +1,8 @@
 package main.java.LabExamples.SOLID.solved.DIP;
-
+/**
+ * SMSNotificationService is another implementation of the NotificationService interface.
+ * It handles sending notifications via SMS.
+ */
 public class SMSNotificationService implements NotificationService {
     @Override
     public void sendNotification(String message) {
