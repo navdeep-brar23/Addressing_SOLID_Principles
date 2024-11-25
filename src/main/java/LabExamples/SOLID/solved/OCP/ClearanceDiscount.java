3a2689f0b069016dec665cb7d4ad31e83266d839
navdeep-brar23 <144741084+navdeep-brar23@users.noreply.github.com>
2024-11-25 02:10:45 -0700

    Update ClearanceDiscount.java

--- src/main/java/LabExamples/SOLID/solved/OCP/ClearanceDiscount.java
+++ src/main/java/LabExamples/SOLID/solved/OCP/ClearanceDiscount.java
@@ -1,5 +1,8 @@
 package main.java.LabExamples.SOLID.solved.OCP;
-
+/**
+ * ClearanceDiscount provides a specific implementation of the Discount class.
+ * It applies a 20% discount.
+ */
 public class ClearanceDiscount extends Discount {
     @Override
     public double calculate(double price) {
