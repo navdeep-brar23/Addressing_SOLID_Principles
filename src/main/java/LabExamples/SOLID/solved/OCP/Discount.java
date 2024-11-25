c8e1f2d1c94e9dca91dac95530de2174473fd391
navdeep-brar23 <144741084+navdeep-brar23@users.noreply.github.com>
2024-11-25 02:09:51 -0700

    Update Discount.java

--- src/main/java/LabExamples/SOLID/solved/OCP/Discount.java
+++ src/main/java/LabExamples/SOLID/solved/OCP/Discount.java
@@ -1,5 +1,14 @@
 package main.java.LabExamples.SOLID.solved.OCP;
-
+/**
+ * Discount is an abstract class that defines a contract for calculating discounts.
+ * Subclasses can provide specific implementations for different discount types.
+ */
 public abstract class Discount {
+     /**
+     * Calculates the discount based on the price.
+     * 
+     * @param price The original price of the product.
+     * @return The discounted price.
+     */
     public abstract double calculate(double price);
 }
