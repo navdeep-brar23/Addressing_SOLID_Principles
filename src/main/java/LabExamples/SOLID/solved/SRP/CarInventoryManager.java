0a0f5d7fc8f96b0c5022aa5cefe128be4122699e
navdeep-brar23 <144741084+navdeep-brar23@users.noreply.github.com>
2024-11-25 02:13:18 -0700

    Update CarInventoryManager.java

--- src/main/java/LabExamples/SOLID/solved/SRP/CarInventoryManager.java
+++ src/main/java/LabExamples/SOLID/solved/SRP/CarInventoryManager.java
@@ -1,5 +1,8 @@
 package main.java.LabExamples.SOLID.solved.SRP;
-
+/**
+ * CarInventoryManager is responsible for managing inventory operations.
+ * This class adheres to the Single Responsibility Principle (SRP) by focusing solely on inventory management tasks.
+ */
 public class CarInventoryManager {
     public void addCar(String carModel, double price) {
         System.out.println("Car added: " + carModel + ", Price: $" + price);
