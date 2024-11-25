9f61ea4d51763174cc049ddea32a46abb7b36675
navdeep-brar23 <144741084+navdeep-brar23@users.noreply.github.com>
2024-11-25 02:14:22 -0700

    Update Main.java

--- src/main/java/LabExamples/SOLID/solved/SRP/Main.java
+++ src/main/java/LabExamples/SOLID/solved/SRP/Main.java
@@ -1,5 +1,9 @@
 package main.java.LabExamples.SOLID.solved.SRP;
 
+/**
+ * Main class to demonstrate the solved implementation of the Single Responsibility Principle (SRP).
+ * Each class (CarInventoryManager, CarSalesManager, InvoiceManager) focuses on a specific responsibility.
+ */
 public class Main {
     public static void main(String[] args) {
         CarInventoryManager inventoryManager = new CarInventoryManager();
