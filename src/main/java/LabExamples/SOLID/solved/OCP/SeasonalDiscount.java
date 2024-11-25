f3b4429cf2195d33a344c174c8d64fe05180cf5c
navdeep-brar23 <144741084+navdeep-brar23@users.noreply.github.com>
2024-11-25 02:10:22 -0700

    Update SeasonalDiscount.java

--- src/main/java/LabExamples/SOLID/solved/OCP/SeasonalDiscount.java
+++ src/main/java/LabExamples/SOLID/solved/OCP/SeasonalDiscount.java
@@ -1,5 +1,8 @@
 package main.java.LabExamples.SOLID.solved.OCP;
-
+/**
+ * SeasonalDiscount provides a specific implementation of the Discount class.
+ * It applies a 10% discount.
+ */
 public class SeasonalDiscount extends Discount {
     @Override
     public double calculate(double price) {
