04ee5bcf2357717a25a90fc29077784db17e2969
navdeep-brar23 <144741084+navdeep-brar23@users.noreply.github.com>
2024-11-25 02:08:56 -0700

    Update CarNotificationManager.java

--- src/main/java/LabExamples/SOLID/solved/DIP/CarNotificationManager.java
+++ src/main/java/LabExamples/SOLID/solved/DIP/CarNotificationManager.java
@@ -1,12 +1,24 @@
 package main.java.LabExamples.SOLID.solved.DIP;
-
+/**
+ * CarNotificationManager is responsible for notifying about car sales.
+ * It depends on the NotificationService interface, adhering to the Dependency Inversion Principle.
+ */
 public class CarNotificationManager {
     private NotificationService notificationService;
-
+ /**
+     * Constructor injects the NotificationService dependency.
+     * This allows for flexibility in choosing the notification method.
+     * 
+     * @param notificationService An implementation of the NotificationService interface.
+     */
     public CarNotificationManager(NotificationService notificationService) {
         this.notificationService = notificationService;
     }
-
+ /**
+     * Sends a notification for a car sale using the injected NotificationService.
+     * 
+     * @param carModel The model of the car that was sold.
+     */
     public void notifySale(String carModel) {
         notificationService.sendNotification("Car sold: " + carModel);
     }
